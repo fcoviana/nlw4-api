e435975486b456e5a22e9f1de470ff9da557af05
Francisco Viana <franciscoviana862@gmail.com>
2021-02-24 19:10:43 -0300

    feat: add routes surveys

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import  UserController from "./controllers/UserController";
+import  SurveysController from "./controllers/SurveysController";
 
 const router = Router();
 
 router.post("/api/users", UserController.create);
+router.post("/api/surveys", SurveysController.create);
+router.get("/api/surveys", SurveysController.show);
 
 router.get("/api/users", (request, response) => {
   return response.send().json({ da: "sdsd" });
