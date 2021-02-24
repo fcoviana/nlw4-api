e33da42b6392363a6a1cddb404e169befd824f72
Francisco Viana <franciscoviana862@gmail.com>
2021-02-24 19:04:10 -0300

    feat: add controller surveys

---
+++ src/controllers/SurveysController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import SurveysRepository from '../repositories/SurveysRepository';
+
+
+class SurveysController {
+ async create(request: Request, response: Response) {
+  const { title, description } = request.body;
+  const surveysRepository = getCustomRepository(SurveysRepository);
+  const survey = surveysRepository.create({ title, description });
+
+  await surveysRepository.save(survey);
+
+  return response.status(201).json(survey);
+ }
+ 
+ async show(request: Request, response: Response) {
+  const surveysRepository = getCustomRepository(SurveysRepository);
+  const all = await surveysRepository.find();
+
+  return response.status(200).json(all);
+ }
+}
+
+export default new SurveysController();
