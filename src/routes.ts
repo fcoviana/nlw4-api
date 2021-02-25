7b9200c6e5cf442156626dd7a6ec1b1692f8b87d
Francisco Viana <franciscoviana862@gmail.com>
2021-02-25 19:37:43 -0300

    feat: add SendMail routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import UserController from './controllers/UserController';
 import SurveysController from './controllers/SurveysController';
+import SendMailController from './controllers/SendMailController';
 
 const router = Router();
 
 router.post('/api/users', UserController.create);
 router.post('/api/surveys', SurveysController.create);
 router.get('/api/surveys', SurveysController.show);
+router.post('/api/sendMail', SendMailController.execute);
 
 router.get('/api/users', (request, response) => {
   return response.send().json({ da: 'sdsd' });
