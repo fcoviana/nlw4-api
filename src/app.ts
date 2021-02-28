c001133c9b079852a6359d619b3c41762aabd343
Francisco Viana <franciscoviana862@gmail.com>
2021-02-28 12:04:07 -0300

    feat: add validaton AppErrors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Response} from 'express';
+import 'express-async-errors';
 import createConnection from './database';
 import router from './routes';
+import { AppError } from './utils/errors/AppErrors';
 
 createConnection();
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, _, response: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      error: err.error,
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    error: 'InternalServerError',
+    message: 'Error interno',
+  });
+});
 
 export default app;
