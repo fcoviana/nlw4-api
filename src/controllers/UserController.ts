f1553a21507e38fd16bbc274a5688565902e53aa
Francisco Viana <franciscoviana862@gmail.com>
2021-02-28 12:07:16 -0300

    refacotor: add yup in UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,9 +2,21 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../repositories/UsersRepository';
 
+import * as Yup from 'yup';
+import { AppError } from '../utils/errors/AppErrors';
+
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Nome é obrigatório'),
+      email: Yup.string().email().required('E-mail deve ser válido'),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      throw new AppError('Validation Failed', 400, 'BadRequest');
+    }
 
     const usersRepository = getCustomRepository(UsersRepository);
 
@@ -13,10 +25,7 @@ class UserController {
     });
 
     if (userAlreadyExists) {
-      return response.status(400).json({
-        error: 'BadRequest',
-        message: 'User already exists',
-      });
+      throw new AppError('User already exists', 400, 'BadRequest');
     }
 
     const user = usersRepository.create({ name, email });
