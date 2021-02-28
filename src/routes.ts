553d73311c8699a8f4675855645b0d00a4140dd9
Francisco Viana <franciscoviana862@gmail.com>
2021-02-28 12:06:06 -0300

    feat: add routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import UserController from './controllers/UserController';
 import SurveysController from './controllers/SurveysController';
 import SendMailController from './controllers/SendMailController';
+import AnswerController from './controllers/AnswerController';
+import NpsController from './controllers/NpsController';
 
 const router = Router();
 
@@ -9,6 +11,8 @@ router.post('/api/users', UserController.create);
 router.post('/api/surveys', SurveysController.create);
 router.get('/api/surveys', SurveysController.show);
 router.post('/api/sendMail', SendMailController.execute);
+router.get('/api/answers/:value', AnswerController.execute);
+router.get('/api/nps/:survey_id', NpsController.execute);
 
 router.get('/api/users', (request, response) => {
   return response.send().json({ da: 'sdsd' });
